808d5c9e02d385c3b782488bc804117b867d6f4b
ronnelreposo <ronnel.reposo@gmail.com>
2017-11-08 06:38:31 -0800

    optional fhr and nsp

--- cardio/cardio/MainWindow.xaml.cs
+++ cardio/cardio/MainWindow.xaml.cs
@@ -16,65 +16,140 @@ namespace cardio
 {
     public partial class MainWindow : ModernWindow
     {
-        public MainWindow () {
-
+        public MainWindow ()
+        {
             InitializeComponent();
 
-            var sClassifyClicked = FromEventPattern(classify_button, "Click").Select(evt => ( evt.Sender as Button ));
-
-            var sClickDisabledButon = sClassifyClicked.Select(button => { button.IsEnabled = false; return button; });
-
-            sClickDisabledButon.Subscribe(async button =>
-                {
-                    const int PlaceVal = 3;
-
-                    var lb_convToSec = Round(minToSec(getDoubleValue(lb_tb)), PlaceVal);
-                    var lb_max = 5;
-                    var ac_max = 30;
-                    var fm_max = 600;
-                    var uc_max = 30;
-                    var dl_max = 20;
-                    var normInputs = new[] {
-                        minmax(lb_convToSec, lb_max, 1),
-                        minmax(getDoubleValue(ac_tb), ac_max),
-                        minmax(getDoubleValue(fm_tb), fm_max),
-                        minmax(getDoubleValue(uc_tb), uc_max),
-                        minmax(getDoubleValue(dl_tb), dl_max),
-                        getDoubleValue(ds_tb),
-                        minmax(getDoubleValue(dp_tb), lb_max)
-                    };
-
-                    Converter<double, double> roundAtPlaceVal = x => Round(x, PlaceVal);
-
-                    var roundedNormInputs = ConvertAll(normInputs, roundAtPlaceVal);
-
-                    var outputControls = new[] {
-                        a_rec, b_rec, c_rec,
-                        d_rec, e_rec, ad_rec,
-                        de_rec, ld_rec, fs_rec,
-                        susp_rec, normal_rec,
-                        suspect_rec, phato_rec };
-
-                    Func<double, double> minmaxPercent = x => minmax(x, 1, -1) * 100;
-
-                    var outputs = Classify(minmaxPercent, roundedNormInputs);
-
-                    var clearedOutputControls = outputControls.Select(rec => rec.ResetWidth());
-
-                    Func<Tuple<double, Rectangle>, Task<Rectangle>> progress = valueAndControl =>
-                     ProgressAsync(ToInt16(valueAndControl.Item1), valueAndControl.Item2, EaseInOutCubic);
-
-                    var progressed = outputs.Zip(clearedOutputControls, Tuple.Create).Select(progress);
-
-                    (await WhenAll(progressed))
-                    .ToObservable()
-                    .Take(1)
-                    .Subscribe(_ => button.IsEnabled = true);
-                });
+            setUpReactiveEngine();
         } /* end constructor. */
 
+        void setUpReactiveEngine ()
+        {
+            var sClassifyClicked = from evt in FromEventPattern(classify_button, "Click")
+                                   select evt.Sender as Button;
+
+            var sClickDisabledButon = from button in sClassifyClicked
+                                      let fhrSelected = fhrClass_cb.IsChecked == true
+                                      let nspSelected = nspClass_cb.IsChecked == true
+                                      where fhrSelected || nspSelected
+                                      select disableButton(button);
+
+            var sClickToClassifyOutputRecord = from button in sClickDisabledButon
+                                               let PlaceVal = 3
+                                               let inputControlVector =
+                                                new[] { lb_tb, ac_tb, fm_tb, uc_tb, dl_tb, ds_tb, dp_tb }
+                                               let outputControlVector =
+                                                new[] {
+                                                    a_rec, b_rec, c_rec, d_rec, e_rec, ad_rec, de_rec, ld_rec,
+                                                    fs_rec, susp_rec, normal_rec, suspect_rec, phato_rec }
+                                               let clearedOutputControlVector =
+                                                from rectangle in outputControlVector
+                                                select rectangle.ResetWidth()
+                                               let normInputs = encodeInputs(inputControlVector, PlaceVal)
+                                               let outputVector = computeVectorOutputs(inputVector: normInputs, placeVal: PlaceVal)
+                                               select new
+                                               {
+                                                   Button = button,
+                                                   InputControlsVector = inputControlVector,
+                                                   OutputControlsVector = clearedOutputControlVector.ToArray(),
+                                                   OutputVector = outputVector
+                                               };
+
+            var sClickToFhr = from record in sClickToClassifyOutputRecord
+                              let fhrIsChecked = fhrClass_cb.IsChecked == true
+                              let fhrClass = 10
+                              where fhrIsChecked
+                              select new
+                              {
+                                  Button = record.Button,
+                                  ProgressedOutputVector = record
+                                    .OutputVector
+                                    .Take(fhrClass)
+                                    .Zip(record.OutputControlsVector.Take(fhrClass), Tuple.Create)
+                                    .Select(progress)
+                              };
+
+            var sClickToNsp = from record in sClickToClassifyOutputRecord
+                              let nspIsChecked = nspClass_cb.IsChecked == true
+                              let fhrClass = 10
+                              where nspIsChecked
+                              select new
+                              {
+                                  Button = record.Button,
+                                  ProgressedOutputVector = record
+                                    .OutputVector
+                                    .Skip(fhrClass)
+                                    .Zip(record.OutputControlsVector.Skip(fhrClass), Tuple.Create)
+                                    .Select(progress)
+                              };
+
+            sClickToFhr
+                .Subscribe(async record =>
+                    ( await WhenAll(record.ProgressedOutputVector) )
+                    .ToObservable()
+                    .Take(1)
+                    .Subscribe(_ => record.Button.IsEnabled = true));
+
+            sClickToNsp
+                .Subscribe(async record =>
+                    ( await WhenAll(record.ProgressedOutputVector) )
+                    .ToObservable()
+                    .Take(1)
+                    .Subscribe(_ => record.Button.IsEnabled = true));
+
+        } /* end setUpReactiveEngine. */
+
+        Button disableButton (Button button)
+        {
+            button.IsEnabled = false;
+            return button;
+        }
+
         /// <summary>
-        /// Converts minutes to second.
+        /// Encodes TextBox Inputs.
+        /// </summary>
+        /// <param name="inputTexboxVector">Input TextBox vector.</param>
+        /// <param name="placeVal">Place value.</param>
+        /// <returns>encoded input vector.</returns>
+        double[] encodeInputs (TextBox[] inputTexboxVector, int placeVal = 3)
+        {
+            var lb_convToSec = Round(minToSec(getDoubleValue(lb_tb)), placeVal);
+            var lb_max = 5;
+            var ac_max = 30;
+            var fm_max = 600;
+            var uc_max = 30;
+            var dl_max = 20;
+            return new[] {
+                minmax(lb_convToSec, lb_max, 1),
+                minmax(getDoubleValue(ac_tb), ac_max),
+                minmax(getDoubleValue(fm_tb), fm_max),
+                minmax(getDoubleValue(uc_tb), uc_max),
+                minmax(getDoubleValue(dl_tb), dl_max),
+                getDoubleValue(ds_tb),
+                minmax(getDoubleValue(dp_tb), lb_max)
+            };
+        } /* end normInputs. */
+
+        /// <summary>
+        /// Computes the output vector.
+        /// </summary>
+        /// <param name="inputVector">The input vector.</param>
+        /// <param name="placeVal">place value.</param>
+        /// <returns>output vector.</returns>
+        double[] computeVectorOutputs (double[] inputVector, int placeVal = 3)
+        {
+            Converter<double, double> roundAtPlaceVal = x => Round(x, placeVal);
+            var roundedNormInputs = ConvertAll(inputVector, roundAtPlaceVal);
+            Func<double, double> minmaxPercent = x => minmax(x, 1, -1) * 100;
+
+            return Classify(minmaxPercent, roundedNormInputs);
+        } /* end compute vector outputs. */
+
+        Task<Rectangle> progress(Tuple<double, Rectangle> valueAndControl) =>
+            ProgressAsync(ToInt16(valueAndControl.Item1), valueAndControl.Item2, EaseInOutCubic);
+
+        /// <summary>
+        /// Converts minute to second.
         /// </summary>
         /// <param name="min">minute value.</param>
         /// <returns>converted minutes value.</returns>
