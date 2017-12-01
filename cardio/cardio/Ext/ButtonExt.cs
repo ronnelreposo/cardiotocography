3309308ea1f8657cfa98b47bb3bab342765cb460
ronnelreposo <ronnel.reposo@gmail.com>
2017-12-01 19:13:39 +0800

    Add ButtonExt
    Adding ButtonExt for the App

---
+++ cardio/cardio/Ext/ButtonExt.cs
@@ -0,0 +1,38 @@
+﻿using System.Windows.Controls;
+
+namespace cardio.Ext
+{
+    /// <summary>
+    /// Represents Button Extension
+    /// </summary>
+    static class ButtonExt
+    {
+        /// <summary>
+        /// Disables the button
+        /// </summary>
+        /// <param name="button">Given button to be disbaled</param>
+        /// <returns>button</returns>
+        internal static Button Disable (this Button button)
+        {
+            if ( !button.IsEnabled ) return button;
+
+            button.IsEnabled = false;
+
+            return button;
+        }
+
+        /// <summary>
+        /// Enables the button
+        /// </summary>
+        /// <param name="button">Given button to enabled</param>
+        /// <returns>button</returns>
+        internal static Button Enable (this Button button)
+        {
+            if ( button.IsEnabled ) return button;
+
+            button.IsEnabled = true;
+
+            return button;
+        }
+    }
+}
